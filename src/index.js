6e37888011d7f21f78d5fc3c836184efe8d86e12
McLizzerson <liesbeth.fr@gmail.com>
2023-10-06 15:19:48 +0200

    Innitialize Sentry

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
 import express from "express";
+import * as Sentry from "@sentry/node";
+import "dotenv/config";
 
 const app = express();
+
+// Initialize Sentry
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// ------------------------------------------------
+// ------------------------------------------------
+// Rest of our app here
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+// ------------------------------------------------
+// ------------------------------------------------
+
+// Error handling
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
