b53ce4675037260d1cd9290f361e321ec1254443
McLizzerson <liesbeth.fr@gmail.com>
2023-10-11 14:56:21 +0200

    Add throw error to delete event

--- src/services/events/deleteEvent.js
+++ src/services/events/deleteEvent.js
@@ -1,14 +1,17 @@
 import eventData from "../../data/events.json" assert { type: "json" };
 
 const deleteEvent = (id) => {
-  const eventIndex = eventData.events.findIndex((event) => event.id === id);
+  const eventIndex = eventData.events.findIndex(
+    (event) => String(event.id) === String(id)
+  );
+
+  if (eventIndex === -1) {
+    throw new Error(`Category with id ${id} was not found!`);
+  }
 
   eventData.events.splice(eventIndex, 1);
 
   return id;
 };
-
-const example = deleteEvent(1);
-console.log(example);
 
 export default deleteEvent;
