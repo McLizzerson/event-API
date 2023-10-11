1867689619987d9853a9d88002401e004423b8f4
McLizzerson <liesbeth.fr@gmail.com>
2023-10-11 15:12:50 +0200

    Add error to delete user

--- src/services/users/deleteUser.js
+++ src/services/users/deleteUser.js
@@ -1,14 +1,17 @@
 import userData from "../../data/users.json" assert { type: "json" };
 
 const deleteUser = (id) => {
-  const userIndex = userData.users.findIndex((user) => user.id === id);
+  const userIndex = userData.users.findIndex(
+    (user) => String(user.id) === String(id)
+  );
+
+  if (userIndex === -1) {
+    throw new Error(`User with id ${id} was not found!`);
+  }
 
   userData.users.splice(userIndex, 1);
 
   return id;
 };
-
-const example = deleteUser(1);
-console.log(example);
 
 export default deleteUser;
